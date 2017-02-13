33e946b8ee1dd0070818aaca5b7d11ae9a0eca9e
David Huang <yam7611@yahoo.com.tw>
2017-02-13 20:16:13 +1100

    make it better3

--- models/todo.js
+++ models/todo.js
@@ -6,9 +6,8 @@ var mongoose = require('mongoose'),
 var TodoSchema  = new Schema(
     {
   name: String
-  account: String
-  password: String
-  
+
+
 });
 
 mongoose.model('employees', TodoSchema);
