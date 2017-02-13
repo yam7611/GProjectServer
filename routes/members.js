1aac60c17c6381a15bbea3e7a2cf3d2c1b5cd4e6
David Huang <yam7611@yahoo.com.tw>
2017-02-13 21:40:50 +1100

    make it better3

--- routes/members.js
+++ routes/members.js
@@ -17,20 +17,15 @@ exports.
 
 exports.findById = function(req,res){
 
-    res.send("req is:"+req)
+    
 
-
-    // Todo.findOne({account:account},function(err,res){
-    //     console.log(res.name);
-    // })
-
-    // Todo.findById( req.params.id, function( err, todo ) {
-    //         if (err) {
-    //             res.send('Error occurred');
-    //             return console.log(err);
-    //         }
-    //         res.send(todo);
-    // });
+    Todo.findById( req.params.id, function( err, todo ) {
+            if (err) {
+                res.send('Error occurred');
+                return console.log(err);
+            }
+            res.send(todo);
+    });
 };
 
 exports.newTodo = function(req,res){
