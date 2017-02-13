e1f91a28f6af08cf1945e24ec16da0ec9657a2db
David Huang <yam7611@yahoo.com.tw>
2017-02-14 10:47:08 +1100

    make it better3

--- routes/members.js
+++ routes/members.js
@@ -30,8 +30,7 @@ exports.longinToSystem = function(req,res){
     const username = req.param("account");
     const password = req.param("password");
     res.send('gotologinsystem');
-    var query = Todo.where({account :username});
-    query.findOne(function(e,o){
+    Todo.findOne({account: username},function(e,o){
         res.send('into findOne');
         if (o == null){
             //callback('user not found');
