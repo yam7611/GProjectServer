d3d65572edfcfbfd6fa89c35c4248c966ab2ea3d
David Huang <yam7611@yahoo.com.tw>
2017-02-14 13:54:17 +1100

    make it better3

--- routes/members.js
+++ routes/members.js
@@ -53,7 +53,7 @@ exports.longinToSystem = function(req,res){
 exports.fetchDataFromServer = function(req,res){
     var request = require('request');
     //res.send('go!')
-    req("http://115.146.91.233/api/task-specifications",function(err,response,body){
+    request("http://115.146.91.233/api/task-specifications",function(err,response,body){
         if (err){
             res.send('error')
         }
