765cfd97f38b85411021f5ffe2b318b7e9dac39b
David Huang <yam7611@yahoo.com.tw>
2017-02-18 12:39:47 +1100

    v4

--- routes/members.js
+++ routes/members.js
@@ -1,8 +1,8 @@
 
 
 var mongoose = require('mongoose'),
-    Todo = mongoose.model('members');
-    //TodoRecord = mongoose.model('records')
+    Todo = mongoose.model('members'),
+    TodoRecord = mongoose.model('records');
 
     //,Address = mongoose.model('Address');
 
@@ -89,20 +89,20 @@ exports.fetchDataFromServer = function(req,res){
     });
 };
 
-// exports.writeDataToDatabase = function(req,res){
+exports.writeDataToDatabase = function(req,res){
 
-//     var record = new TodoRecord(req.body);
+    var record = new TodoRecord(req.body);
 
-//     record.save(function(err){
-//         if(err){
-//             //res.send('error')
-//             res.send.json({"message":"fail to upload"});
-//         } 
+    record.save(function(err){
+        if(err){
+            //res.send('error')
+            res.send.json({"message":"fail to upload"});
+        } 
 
-//         res.send.json({"message":"successfully upload"});
-//     });
+        res.send.json({"message":"successfully upload"});
+    });
 
-// };
+};
 
 exports.update = function(req,res){
     Todo.findById( req.params.id, function( err, todo ) {
