912a76ae1c8c5278a7cd5ab3f5d7f762eb3524d5
David Huang <yam7611@yahoo.com.tw>
2017-02-18 12:21:52 +1100

    v4

---
+++ models/records.js
@@ -0,0 +1,16 @@
+
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var TodoSchema  = new Schema(
+    {
+  block:String,
+  start_time: String,
+  end_time: String
+  
+
+});
+
+mongoose.model('records', TodoSchema);
+
--- routes/members.js
+++ routes/members.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose'),
     Todo = mongoose.model('members'),
-    TodoRecord = mongoose('records')
+    TodoRecord = mongoose.model('records')
 
     //,Address = mongoose.model('Address');
 
